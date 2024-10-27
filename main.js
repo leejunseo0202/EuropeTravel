eaebc172767717e71c7cb03256903833078ff469
leejunseo0202 <leejunseo010@gmail.com>
2024-10-27 23:22:01 +0900

    add bottom bar

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const body = document.querySelector("body"),
     category = body.querySelector(".category"),
     countries = body.querySelectorAll(".country_button"),
-    city_image = body.querySelector(".city_image");
+    city_image = body.querySelector(".city_image"),
+    city_image_h1 = body.querySelector(".city_image_detail h1"),
+    city_image_span = body.querySelector(".city_image_detail span");
+
 
 /*-----slick slide-----*/
 $('.category_image').slick({
@@ -42,7 +45,21 @@ countries.forEach(country=> {
 });
 
 $('.category_image').on('afterChange', function(event, slick, currentSlide, nextSlide){   
-    console.log(currentSlide); 
     city_image.src = "https://cdn.jsdelivr.net/gh/leejunseo0202/EuropeTravel@master/image/city_"
     + category.classList[1] +"0" + (currentSlide+1) + ".jpg";
+
+    const city_detail = new Map([["로마", "트레비 분수, 판테온, 포로 로마노..."],
+                                ["바티칸", "성 베드로 대성전, 시스티나 성당, 바티칸 오벨리스크..."],
+                                ["나폴리", "누우보 성, 산텔모 성, 델로보 성..."],
+                                ["폼페이", "폼페이 유적..."],
+                                ["소렌토", "타소 광장, 해안절벽..."],
+                                ["피렌체", "피사의 사탑, 피렌체 두우모, 우피치 미술관..."],
+                                ["베네치아", "산 마르코 광장, 산 마르코 대성당, 부라노 섬..."],
+                                ["인스부르크", "황금지붕, 호프부르크 왕궁, 노르트케테..."],
+                                ["뮌헨", "옥토버페스트, 님펜부르크 궁전, 성 피터 교회..."],
+                                ["잘츠부르크", "호엔잘츠부르크 요새, 묀히스베르크 전망대, 할슈타트..."],
+                                ["비엔나", "벨베데레 궁전, 케른트너 거리, 슈테판 대성당..."],
+                                ["프라하", "카를교, 프라하 성, 체스키크룸로프..."]]);
+    city_image_h1.innerText = (city_name.get(category.classList[1]))[currentSlide];
+    city_image_span.innerText = (city_detail.get((city_name.get(category.classList[1]))[currentSlide]));
 });
